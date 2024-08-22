960306344756ff46c23505bb89c33ddd14a04269
Thiago soares <104157930+0thiago@users.noreply.github.com>
2024-08-21 21:33:37 -0300

    polishing project

--- Assets/Scripts/UI/MainMenuScreen/Buttons/NewGame.cs
+++ Assets/Scripts/UI/MainMenuScreen/Buttons/NewGame.cs
@@ -5,19 +5,10 @@ using UnityEngine.SceneManagement;
 
 public class NewGame : MonoBehaviour
 {
-    [SerializeField] private MainMenuScreen MainMenuScreenCanvasGroups;
-    //[SerializeField] private float fadingSpeed = 5f;
     private const string GAME_SCENE = "Game";
 
     public void OnClickHandle()
     {
-        /* In case it needs some fade out
-        float currentAlpha = MainMenuScreenCanvasGroups.GetComponent<CanvasGroup>().alpha;
-        float fadingAlpha = Mathf.Lerp(currentAlpha, 0f, fadingSpeed);
-
-        MainMenuScreenCanvasGroups.SetMainMenuScreenAlpha(fadingAlpha);
-        */
-
         SceneManager.LoadScene(GAME_SCENE);
     }
 }
