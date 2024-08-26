00515c043d4258dc60ebc6691537ca883ab20f24
Leonardo Gomes <leogomesp10@gmail.com>
2024-08-26 20:40:15 -0300

    [27] Add simple product restock

--- Assets/Scripts/Grid/ProductGridConfiguration.cs
+++ Assets/Scripts/Grid/ProductGridConfiguration.cs
@@ -4,12 +4,9 @@ using UnityEngine;
 public class ProductGridConfiguration : ScriptableObject
 {
     [SerializeField] private Vector2Int gridSize;
-    [SerializeField] private Vector2 cellGap;
     [SerializeField] private Vector2 cellSize;
 
     public Vector2Int GridSize => gridSize;
-
-    public Vector2 CellGap => cellGap;
 
     public Vector2 CellSize => cellSize;
 }
